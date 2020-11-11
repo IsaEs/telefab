152c590345ffdf81c44b3017b1e1c7d5d000b48f
IsaEs <merhaba@isaes.com.tr>
2020-11-11 03:13:07 +0300

    obsufcator and uglifyjs

---
+++ .obsufcator.js
@@ -0,0 +1,38 @@
+module.exports = {
+    "compact":true,
+    "simplify":true,
+    "identifierNamesGenerator":"mangled-shuffled",
+    "identifiersDictionary":[
+      
+    ],
+    "identifiersPrefix":"",
+    "ignoreRequireImports":false,
+  
+    "log":false,
+    "numbersToExpressions":false,
+    "optionsPreset":"low-obfuscation",
+
+    "reservedStrings":[
+      
+    ],
+    "rotateStringArray":true,
+    "seed":0,
+    "selfDefending":false,
+    "shuffleStringArray":true,
+   
+    "sourceMap":false,
+    "sourceMapMode":"separate",
+    "splitStrings":false,
+    "splitStringsChunkLength":10,
+    "stringArray":true,
+    "stringArrayEncoding":[
+      
+    ],
+    "stringArrayWrappersCount":1,
+    "stringArrayWrappersChainedCalls":true,
+    "stringArrayWrappersType":"variable",
+    "stringArrayThreshold":0.75,
+    "target":"browser",
+    "transformObjectKeys":false,
+    "unicodeEscapeSequence":false
+  }
--- README.md
+++ README.md
@@ -8,10 +8,10 @@ Available settings for your telefab.
 
 - telefab-name -- Name of your formId
 - telefab-id : Uniqe teleform HASH_ID for your form
+- data-header: Header for FAB
 
 // TODO customization attributes
 
-- data-company: Header for FAB
 - data-explanation: Header explanation
 - data-main-color: Color of your header
 - data-button-color: Send Button color for
@@ -22,6 +22,7 @@ Available settings for your telefab.
   async src="https://www.telecontact.me/telefab.js"
   telefab-name={{NAME}}
   telefab-id={{HASH_ID}}
+  data-header={{HEADER}}
 />
 
 // Example Usage
@@ -29,6 +30,7 @@ Available settings for your telefab.
   async src="https://www.telecontact.me/telefab.js"
   telefab-name="TestButton"
   telefab-id="W-lx1JJLMKb6"
+  data-header="👋 Telecontact.me"
 />
 ```
 
